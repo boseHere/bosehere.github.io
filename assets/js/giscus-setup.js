6aef360bb953900d73a4aada53c5004e37c01e26
boseHere <47787632+boseHere@users.noreply.github.com>
2025-10-18 19:40:52 +0000

    Deploying to gh-pages from @ boseHere/mayabose.github.io@cfac4f8fb99f0d9093386b9d6f631bfb459f158b 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "boseHere/mayabose",
+    "data-repo": "boseHere/mayabose.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
